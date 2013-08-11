a82a378d712cd6ec0073add311d573fa6ffb30f2
moonmonkey <woodhams@gmail.com>
2013-08-11 16:43:03 +1000

    test

--- data.py
+++ data.py
@@ -1,7 +1,11 @@
-import pickle
+try:
+  from cPickle import dumps, loads
+except ImportError:
+  from pickle import dumps, loads
+
 
 while True:                                     
                                   
         number = pickle.load( open( "save.p", "wb" ) )
         print number
-      
+      	
