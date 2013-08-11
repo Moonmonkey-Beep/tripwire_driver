fba2427283c20020eb17291b51bd3c5d29b557f4
moonmonkey <woodhams@gmail.com>
2013-08-11 14:10:04 +1000

    More

--- python/LDRauto.py
+++ python/LDRauto.py
@@ -1,28 +1,63 @@
-  #!/usr/bin/env python
+#####################################SETUP####################################### 
 
-# Example for RC timing reading for Raspberry Pi
-# Must be used with GPIO 0.3.1a or later - earlier verions
-# are not fast enough!
-
-import RPi.GPIO as GPIO, time, os      
-
-DEBUG = 1
+# Standard setup starts
+import RPi.GPIO as GPIO, time, os
+from time import sleep
+from sys import exit
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN) # this turns the button to input on 23
 
-def RCtime (RCpin):
+# Standard setup ends
+
+def RCtime (RCpin): ## Setup LDR detection
         reading = 0
         GPIO.setup(RCpin, GPIO.OUT)
         GPIO.output(RCpin, GPIO.LOW)
-        time.sleep(0.1)
-
+        time.sleep(0.05)
+ 
         GPIO.setup(RCpin, GPIO.IN)
         # This takes about 1 millisecond per loop cycle
         while (GPIO.input(RCpin) == GPIO.LOW):
                 reading += 1
         return reading
+        
+# define function flashing LED - flash rate defined in the call
+def flashled( str ):
+    GPIO.setup(22, GPIO.OUT) ##Setup LED
+    GPIO.output(22, True)
+    time.sleep(str)
+    GPIO.output(22, False)
+    time.sleep(str)
+    
+    # define function playing sound - file defined in call
+def playsound( str ):
+    bashCommand = str
+    import subprocess
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+   
+# define function
+def laseron():
+    GPIO.setup(17, GPIO.OUT) ##Setup Laser
+    GPIO.output(17, True) ## Laser on
+   
+   # define function
+def laseroff():
+    GPIO.setup(17, GPIO.OUT) ##Setup Laser
+    GPIO.output(17, False) ## Laser on
+
+##################################ACTIVE#####################################
+
+## THE LOWER THE LIGHT CONDITIONS, THE HIGHER THE READING FROM THE PHOTOCELL
+## PLAN TO MAKE THE ALERT LEVEL ADAPTIVE (TWICE AS MUCH DARKNESS AS LAST LIGHT LEVEL INSTEAD OF A FIXED NUMBER)
+## ALSO SHOULD HAVE DIFFERENT TRIGGER WHEN LIGHT LEVEL INCREASES DRASTICLY - BUT NEED TO CALIBRATE TO AVOICE BEING TRIGGERED BY LIGHT SWITCHES
+## NEED TO CHECK FOR DARKNESS ON FIRST RUN AND LAUNCH CALIBRATION MODE
 
 
+while True:
 
-While True:                                     
-        lightlevel = RCtime(18)   # Read RC timing using pin #18
-		print lightlevel
+
+    Lightlevel = RCtime(18)
+	print lightlevel
+
+    
