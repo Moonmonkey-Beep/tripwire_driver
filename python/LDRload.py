b9c13d5a9387273ef54cf1d05e2324383abec9a1
moonmonkey <woodhams@gmail.com>
2013-08-11 14:52:39 +1000

    more

--- python/LDRload.py
+++ python/LDRload.py
@@ -26,4 +26,5 @@ if True:
         import pickle
         
         favorite_color = pickle.load( open( "save.p", "rb" ) )
+        print favorite_color
         # favorite_color is now { "lion": "yellow", "kitty": "red" }	print lightlevel
