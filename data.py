82933f59a0fdb158c63cdaf6ab38d57817437bb0
moonmonkey <woodhams@gmail.com>
2013-08-18 11:52:51 +1000

    Test

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 import pickle
+while True:   
 
-
-try:
-  number = pickle.load( open( "save.p", "rb" ) )
-  print number
-except EOFError:
-  raise StopIteration
+	try:
+		number = pickle.load( open( "save.p", "rb" ) )
+		print number
+	except EOFError:
+		raise StopIteration
