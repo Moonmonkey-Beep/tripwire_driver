7f0e7498d7afdafa522aa7131f22c09a9b2d9847
moonmonkey <woodhams@gmail.com>
2013-08-18 11:36:53 +1000

    More

--- newdata.py
+++ newdata.py
@@ -1,6 +1,6 @@
 
 
-if True:                                     
+While True:                                     
         # Load the dictionary back from the pickle file.
         import pickle
         
