6433f89738d9c9a5ae60cedcd755f55369584c04
moonmonkey <woodhams@gmail.com>
2013-08-31 14:15:15 +1000

    TEST

--- main.py
+++ main.py
@@ -57,60 +57,76 @@ def standbymode():
 
     while True:
 
-	#flashled(1.5); ## This is when waiting button press to arm system
-		if ( GPIO.input(23) == False ):
-			playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/button.wav");       
-			armtripwire()    
+		#flashled(1.5); ## This is when waiting button press to arm system
+			if ( GPIO.input(23) == False ):
+				playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/button.wav");       
+				armtripwire()    
 	    
 def armtripwire():
-		pickle.dump( "Armed", open( "/opt/ninja/drivers/save.p", "wb" ) )
-		playchirps = 1
-		playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/warmup.wav");
-		laseron(); # turn laser on pin #17
-		Alertlevel = 10000 ## set the base level very high for the first run to prevent false alarms
-		Alarmcount = 0 ## how many times to play the alarm when triggered (makes sure Ninja Cloud detects it)
-		while True:
-
-
-		    Lightlevel = RCtime(18)
-
-		    if (Alarmcount > 0):
-		    	pickle.dump( "Alarm", open( "/opt/ninja/drivers/save.p", "wb" ) )
-		    	print Alarmcount
-		    	Alarmcount = Alarmcount - 1
-		    else:
-		    	pickle.dump( "Armed", open( "/opt/ninja/drivers/save.p", "wb" ) )	    
-
-
-		    Alertlevel = 2000 ## this is the ammount of darkness which triggers the alarm
-
-
-		    if (playchirps > 0): ## makes sure sound is only played once
-		        playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/activated1.wav");
-		        playchirps = playchirps - 1
-
-
-
-		    if (Lightlevel > Alertlevel):
-
-		        # this is when intruder is detected
-		        #flashled(.1);  
-		        Alarmcount = 10 ## how many times to play the alarm when triggered (makes sure Ninja Cloud detects it)
-		        if (Alarmcount >= 0):
-		        	print Alarmcount
-		        	Alarmcount = Alarmcount - 1
-
-
-		        playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/detected.wav");
-		        #laseroff()
-
-		    else:
-		       if ( GPIO.input(23) == False ):
-		            #print "button pressed"
-		            playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/button.wav");
-		            laseroff()
-		            playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/deactivated.wav");
-		            standbymode()
+	print "Arming Tripwire"
+	pickle.dump( "Armed", open( "/opt/ninja/drivers/save.p", "wb" ) )
+	playchirps = 1
+	playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/warmup.wav");
+	laseron(); # turn laser on pin #17
+	Alertlevel = 10000 ## set the base level very high for the first run to prevent false alarms
+	Alarmcount = 0 ## how many times to play the alarm when triggered (makes sure Ninja Cloud detects it)
+	while True:
+	    print Alarmcount
+	
+	    Lightlevel = RCtime(18)
+	
+	    if (Alarmcount > 0):
+	    	pickle.dump( "Alarm", open( "/opt/ninja/drivers/save.p", "wb" ) )
+	    	print Alarmcount
+	    	Alarmcount = Alarmcount - 1
+	    else:
+	    	pickle.dump( "Armed", open( "/opt/ninja/drivers/save.p", "wb" ) )	    
+	    	
+	       
+	    Alertlevel = 2000 ## this is the ammount of darkness which triggers the alarm
+	
+	
+	    if (playchirps > 0): ## makes sure sound is only played once
+	       
+			playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/activated1.wav");
+			playchirps = playchirps - 1
+			randnumber = randint(1,4) #Inclusive
+	 
+			if (randnumber == 1):
+				playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/activated1.wav");
+				playchirps = playchirps - 1
+			elif (randnumber == 2):
+				playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/activated2.wav");
+				playchirps = playchirps - 1
+			elif (randnumber == 3):
+				playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/activated3.wav");
+				playchirps = playchirps - 1
+			elif (randnumber == 4):
+				playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/activated4.wav");
+				playchirps = playchirps - 1
+	        
+	        
+	 
+	    if (Lightlevel > Alertlevel):
+	        
+	        # this is when intruder is detected
+	        #flashled(.1);  
+	        Alarmcount = 20 ## how many times to play the alarm when triggered (makes sure Ninja Cloud detects it)
+	        if (Alarmcount >= 0):
+	        	print Alarmcount
+	        	Alarmcount = Alarmcount - 1
+	  
+	        
+	        playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/detected.wav");
+	        #laseroff()
+	
+	    else:
+	       if ( GPIO.input(23) == False ):
+	            print "button pressed"
+	            playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/button.wav");
+	            laseroff()
+	            playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/deactivated.wav");
+	            standbymode()
     
 # define function  
 def AlignLaser(): # align lasers
