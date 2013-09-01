8da76ff2c08f473fe17cb1aa4a0b57bcc94adc1e
moonmonkey <woodhams@gmail.com>
2013-09-01 13:03:07 +1000

    New Sounds

--- StartupScript.py
+++ StartupScript.py
@@ -1,8 +1,19 @@
 import RPi.GPIO as GPIO, time, os
-
+from random import randint
 
 #####################################MAIN####################################### 
 print "running StartupScript"
+
+from random import randint
+randnumber = randint(1,2) #Inclusive
+
+if (randnumber == 1):
+		playsound("sudo aplay /opt/ninja/drivers/tripwire_driver/sounds/hello1.wav");
+
+elif (randnumber == 2):
+		playsound("sudo aplay /opt/ninja/drivers/tripwire_driver/sounds/hello2.wav");
+
+
 bashCommand = "sudo python /opt/ninja/drivers/tripwire_driver/main.py"
 os.system(bashCommand) 
 
