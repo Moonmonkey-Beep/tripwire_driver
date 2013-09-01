91f3c732723ebde9c7685704d2b7576024830125
moonmonkey <woodhams@gmail.com>
2013-09-01 12:21:14 +1000

    Fix

--- StartupScript.py
+++ StartupScript.py
@@ -1,3 +1,6 @@
+import RPi.GPIO as GPIO, time, os
+
+
 #####################################MAIN####################################### 
 print "running StartupScript"
 bashCommand = "sudo python /opt/ninja/drivers/tripwire_driver/main.py"
