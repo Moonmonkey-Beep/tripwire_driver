12c51ebee1b1a87b9264af89c47e96416e59249e
moonmonkey <woodhams@gmail.com>
2013-11-05 18:03:08 +1100

    New PIR Testing

--- PIRTest.py
+++ PIRTest.py
@@ -1,26 +1,14 @@
-  #!/usr/bin/env python
+import time
+import RPi.GPIO as io
+io.setmode(io.BCM)
+	 
+pir_pin = 22
 
-# Example for RC timing reading for Raspberry Pi
-# Must be used with GPIO 0.3.1a or later - earlier verions
-# are not fast enough!
+io.setup(pir_pin, io.IN)         # activate input
+io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
+	 
+while True:
+	if io.input(pir_pin):
+		print("PIR ALARM!")
 
-import RPi.GPIO as GPIO, time, os      
-
-DEBUG = 1
-GPIO.setmode(GPIO.BCM)
-
-def RCtime (RCpin):
-        reading = 0
-        GPIO.setup(RCpin, GPIO.OUT)
-        GPIO.output(RCpin, GPIO.LOW)
-        time.sleep(0.1)
-
-        GPIO.setup(RCpin, GPIO.IN)
-        # This takes about 1 millisecond per loop cycle
-        while (GPIO.input(RCpin) == GPIO.LOW):
-                reading += 1
-        return reading
-
-while True:                                     
-        lightlevel = RCtime(22)   # Read RC timing using pin #22
-	print lightlevel
+		time.sleep(0.5)
