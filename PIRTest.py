53822c2c3a207a19b69bbd560a5867f302e4b748
moonmonkey <woodhams@gmail.com>
2013-11-05 18:07:03 +1100

    more tests

--- PIRTest.py
+++ PIRTest.py
@@ -1,14 +1,16 @@
 import time
 import RPi.GPIO as io
 io.setmode(io.BCM)
-	 
+
 pir_pin = 22
+door_pin = 23
 
 io.setup(pir_pin, io.IN)         # activate input
 io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
-	 
+
 while True:
 	if io.input(pir_pin):
 		print("PIR ALARM!")
-
-		time.sleep(0.5)
+		if io.input(door_pin):
+			print("DOOR ALARM!")
+			time.sleep(0.5)
