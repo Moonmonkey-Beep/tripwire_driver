ccfadc26b03f62663786fa2692e677982ce240f1
moonmonkey <woodhams@gmail.com>
2014-01-19 11:30:54 +1100

    More tests

--- robot.py
+++ robot.py
@@ -4,7 +4,7 @@
 import RPi.GPIO as GPIO, time, os
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
-GPIO.setup(22, GPIO.IN)
+
 
 import cPickle as pickle
 from random import randint
@@ -32,6 +32,23 @@ def laseron():
 def laseroff():
     GPIO.setup(17, GPIO.OUT) ##Setup Laser
     GPIO.output(17, False) ## Laser on
+
+
+# define function
+def WeaponArm():
+    GPIO.setup(22, GPIO.OUT) ##Setup Laser
+    GPIO.output(22, True) ## Laser on
+   
+   # define function
+def WeaponDisarm():
+    GPIO.setup(22, GPIO.OUT) ##Setup Laser
+    GPIO.output(22, False) ## Laser on
+
+def Attack():
+	playsound("sudo aplay /opt/ninja/drivers/tripwire_driver/sounds/detected.wav");
+	WeaponArm()
+	laseron()
+		
 
 def shutdown():
 	randnumber = randint(1,2) #Inclusive
@@ -69,7 +86,7 @@ def standbymode():
 			if ( GPIO.input(23) == False ):
 				playsound("sudo aplay -q /opt/ninja/drivers/tripwire_driver/sounds/buttonup.wav");      
 				print "button pressed" 
-	 
+	 			attack()
 
 #####################################MAIN####################################### 
 
