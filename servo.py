a836c018358d311f1caf317215b97f337e3785df
moonmonkey <woodhams@gmail.com>
2014-02-23 10:27:57 +1100

    More Servo

--- servo.py
+++ servo.py
@@ -1,21 +1,29 @@
-
-#!/usr/bin/env python<br />
+import RPi.GPIO as GPIO
 import time
-import os
- 
-STEP = 100
-DELAY = 0.5
- 
-def pwm(pin, angle):
-	print "servo[" + str(pin) + "][" + str(angle) + "]"
-	cmd = "echo " + str(pin) + "=" + str(angle) + " &gt; /dev/servoblaster"
-	os.system(cmd)
-	time.sleep(DELAY)
- 
-	while True:
-		for i in range(0, 8):
-			for j in range(1, 249, STEP):
-				pwm(i,j)
-		for i in range(0, 8):
-			for j in range(249, 1, (STEP*-1)):
-				pwm(i,j)
+
+pin = 17
+refresh_period = 0.02
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(pin, GPIO.OUT)
+GPIO.output(pin, True)
+
+while True:
+    for i in range(1, 100):
+        GPIO.output(pin, False)
+        time.sleep(0.001)
+        GPIO.output(pin, True)
+        time.sleep(refresh_period)
+
+    for i in range(1, 100):
+        GPIO.output(pin, False)
+        time.sleep(0.0015)
+        GPIO.output(pin, True)
+        time.sleep(refresh_period)
+
+    for i in range(1, 100):
+        GPIO.output(pin, False)
+        time.sleep(0.002)
+        GPIO.output(pin, True)
+        time.sleep(refresh_period)
