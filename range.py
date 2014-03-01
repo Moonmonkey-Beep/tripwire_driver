449379c26a31030577355569c44b9e8ed776c490
moonmonkey <woodhams@gmail.com>
2014-03-01 12:12:53 +1100

    more

--- range.py
+++ range.py
@@ -5,10 +5,10 @@ import RPi.GPIO as GPIO, time, os
 GPIO.setmode(GPIO.BCM)
 
 
+while True:
 
-
-def laseron():
-    GPIO.setup(11, GPIO.OUT) ##Setup Laser
-    GPIO.output(11, True) ## Laser on
+		def laseron():
+			GPIO.setup(9, GPIO.OUT) ##Setup Laser
+			GPIO.output(9, True) ## Laser on
 
 
